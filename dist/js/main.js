fcd1cf1c177be5fe51280c2ec34650a2634c0e0e
Anastacia Kudriashova <kudriashova.ag@gmail.com>
2021-12-03 11:57:05 +0200

    3.12

--- dist/js/main.js
+++ dist/js/main.js
@@ -28,6 +28,9 @@ $(document).ready(function () {
     ]
   });
 
+
+
+
   const optionsSlickPartners = {
     infinite: true,
     slidesToShow: 6,
@@ -51,42 +54,78 @@ $(document).ready(function () {
   }
   $('.slider-partners .slider').slick(optionsSlickPartners);
 
-  window.addEventListener("resize", function () {
-    if (window.innerWidth <= 768) {
-      $('.slider-partners .slider').slick('unslick');
-      sliderIsLive = false;
-    }
-    else {
-      if (!sliderIsLive) {
-        $('.slider-partners .slider').slick(optionsSlickPartners);
-        sliderIsLive = true;
-      }
-    }
-  });
-  $('.slider-testimonial .slider').slick({
+
+
+
+  // $('.slider-testimonial .slider').slick({
+  //   responsive: [
+  //     {
+  //       breakpoint: 768,
+  //       settings: "unslick"
+  //     }
+  //   ]
+  // });
+
+
+  const optionsSlickScroll = {
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: $('.slider-scroll .prev-arrow'),
+    nextArrow: $('.slider-scroll .next-arrow'),
     responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
       {
         breakpoint: 768,
         settings: "unslick"
       }
     ]
-  });
+  };
+
+  $('.gallery .slider-scroll .slider').slick(optionsSlickScroll);
+
+
+
+  const optionsTestimonialsSlickScroll = {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+      
+      {
+        breakpoint: 768,
+        settings: "unslick"
+      }
+    ]
+  };
+
+
+  $('.testimonials-section .slider-scroll .slider').slick(optionsTestimonialsSlickScroll);
+
+
   sliderIsLive = true;
   window.addEventListener("resize", function () {
     if (window.innerWidth <= 768) {
-      $('.slider-testimonial .slider').slick('unslick');
+      $('.slider-partners .slider').slick('unslick');
+      $('.gallery .slider-scroll .slider').slick('unslick');
+      $('.testimonials-section .slider-scroll .slider').slick('unslick');
       sliderIsLive = false;
     }
     else {
       if (!sliderIsLive) {
-        $('.slider-testimonial .slider').slick({
-          breakpoint: 768,
-          settings: "unslick"
-        });
+        $('.slider-partners .slider').slick(optionsSlickPartners);
+        $('.gallery .slider-scroll .slider').slick(optionsSlickScroll);
+        $('.testimonials-section .slider-scroll .slider').slick();
         sliderIsLive = true;
       }
     }
   });
+
+  
+
 });
 
 
